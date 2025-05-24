8d50988869f676c6fcdf83556cd6e48cd083d198
Alaa Kracah <alaaqaraja2004@gmail.com>
2025-05-24 07:21:46 +0300

    Sdcw

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import LayoutRoute from "./Layout/LayoutRoute";
+import Layout from "./Layout/Layout";
 import "./styles/index.css";
+import Router from "./Router/Router";
 
 function App() {
-  return <LayoutRoute />;
+  return (
+    <LayoutRoute>
+      <Layout>
+        <Router />
+      </Layout>
+    </LayoutRoute>
+  );
 }
 
 export default App;
